00df298328003a6171c0941485e01182f8b62569
Bespi123 <b_antonioesp@hotmail.com>
2024-10-17 18:19:09 -0500

    Add Read satellite data. Update path.

--- tracking/readAzEl.m
+++ tracking/readAzEl.m
@@ -1,46 +1,38 @@
 %% Read Az and El files
+%**************************************************************************
+% AUTHOR: Brayan Espinoza 17/10/2024
+% DESCRIPTION: 
+% This program reads the .frd files which contains the azimuth and elevation
+% angles for the TLRS-3 telescope. The angles were calculated by the computer
+% from the AREL laser estation.
+% IMPORTANT: 
+%
+% *************************************************************************
+
 %Read .sm files
-frdFiles = dir('BRAYAN/*/*e?????????.frd'); %% Read Laser files
+frdFiles = dir('tracking/BRAYAN/*/*e?????????.frd'); %% Read Laser files
 %Read .frd files
 %%Change to your data directory
+
 for i=1:length(frdFiles)
     disp(frdFiles(i).name)
     satData(i) = readFrd(strcat(frdFiles(i).folder,'\',frdFiles(i).name));
 end
 
-%%%Plot read az and elevation Read
-figure()
-subplot(2,1,1)
-plot(satData(1).sod-satData(1).sod(1), satData(1).az); grid on;
-title(satData(1).name);
-xlabel('seconds (s)'); ylabel('Azimuth (deg)');
-subplot(2,1,2)
-plot(satData(1).sod-satData(1).sod(1), satData(1).el); grid on;
-xlabel('seconds (s)'); ylabel('Elevation (deg)');
-
-figure()
-subplot(2,1,1)
-plot(satData(2).sod-satData(2).sod(1), satData(2).az); grid on;
-title(satData(2).name);
-xlabel('seconds (s)'); ylabel('Azimuth (deg)');
-subplot(2,1,2)
-plot(satData(2).sod-satData(2).sod(1), satData(2).el); grid on;
-xlabel('seconds (s)'); ylabel('Elevation (deg)');
-
-figure()
-subplot(2,1,1)
-plot(satData(3).sod-satData(3).sod(1), satData(3).az); grid on;
-title(satData(3).name);
-xlabel('seconds (s)'); ylabel('Azimuth (deg)');
-subplot(2,1,2)
-plot(satData(3).sod-satData(3).sod(1), satData(3).el); grid on;
-xlabel('seconds (s)'); ylabel('Elevation (deg)');
-
-figure()
-subplot(2,1,1)
-plot(satData(4).sod-satData(4).sod(1), satData(4).az); grid on;
-title(satData(4).name);
-xlabel('seconds (s)'); ylabel('Azimuth (deg)');
-subplot(2,1,2)
-plot(satData(4).sod-satData(4).sod(1), satData(4).el); grid on;
-xlabel('seconds (s)'); ylabel('Elevation (deg)');
+%%% Plot azimuth and elevation data for all satellites
+for i = 1:length(satData)
+    figure();
+    sp1 = subplot(2, 1, 1);
+        plot(satData(i).sod - satData(i).sod(1), satData(i).az,'LineStyle','--','LineWidth',2); 
+        grid on;
+        title(satData(i).name);  % Use the satellite name for the title
+        xlabel('Seconds (s)'); 
+        ylabel('Azimuth (deg)');
+    
+    sp2 = subplot(2, 1, 2);
+        plot(satData(i).sod - satData(i).sod(1), satData(i).el,'LineStyle','--','LineWidth',2); 
+        grid on;
+        xlabel('Seconds (s)'); 
+        ylabel('Elevation (deg)');
+    linkaxes([sp1,sp2],'x');
+end
